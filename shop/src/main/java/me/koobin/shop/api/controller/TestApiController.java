e63d341c55e8e68186f453808e5fdef6b7521eb5
tgkoo <jinhoa52@gmail.com>
2021-08-10 20:35:01 +0900

    rest api, swagger 설정 제거
    프로젝트 경험이 부족한데 rest api, swagger를 사용하는 게 부담

--- shop/src/main/java/me/koobin/shop/api/controller/TestApiController.java
+++ shop/src/main/java/me/koobin/shop/api/controller/TestApiController.java
@@ -1,4 +1,4 @@
-package me.koobin.shop.controller;
+package me.koobin.shop.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
---
+++ shop/src/main/java/me/koobin/shop/exception/NotFoundException.java
@@ -0,0 +1,8 @@
+package me.koobin.shop.exception;
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException() {
+        super("리소스를 찾지 못했습니다.");
+    }
+}
