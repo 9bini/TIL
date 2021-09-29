895b01e58f8b73e5dbf88350f6b78c04b5da041a
tgkoo <jinhoa52@gmail.com>
2021-09-29 22:59:10 +0900

    enum 튜토리얼 참고

---
+++ java_tutorial/src/com/koobin/enumerate/Compass.java
@@ -0,0 +1,5 @@
+package com.koobin.enumerate;
+
+public enum Compass {
+  NORTH, SOUTH, EAST, WEST
+}
---
+++ java_tutorial/src/com/koobin/enumerate/Day.java
@@ -0,0 +1,8 @@
+package com.koobin.enumerate;
+
+/**
+ * @author gutaegyun
+ */
+public enum Day {
+  SUNDAY, MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY
+}
